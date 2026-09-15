830f6316bd58f209734bc7c3d9b17c6441e5ec47
Yersy <yersi78@gmail.com>
2026-09-15 10:00:43 -0500

    Carpeta para frontend

---
+++ src/frontend/src/app/presentation/views/clientes/clientes.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-clientes',
+  imports: [],
+  templateUrl: './clientes.html',
+  styleUrl: './clientes.css',
+})
+export class Clientes {}
---
+++ src/frontend/src/app/presentation/views/productos/productos.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-productos',
+  imports: [],
+  templateUrl: './productos.html',
+  styleUrl: './productos.css',
+})
+export class Productos {}
