96a3e7bf0a2579fcfa3dca5618ade9a682c8a4be
nellsix65 <victormallma2004@gmail.com>
2026-09-17 01:00:05 -0500

    slider creado en home

--- src/frontend/src/app/presentation/views/home/home.ts
+++ src/frontend/src/app/presentation/views/home/home.ts
@@ -1,9 +1,36 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-home',
   imports: [],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {}
+export class Home {
+
+currentIndex = 0;
+
+  slides = [
+    '/images/img3.jpg',
+    '/images/img2.jpg',
+    '/images/img1.jpg',
+    '/images/img4.jpg'
+  ];
+
+  btnNext() {
+    this.currentIndex =
+      (this.currentIndex + 1) % this.slides.length;
+  }
+
+  btnPrev() {
+    this.currentIndex =
+      (this.currentIndex - 1 + this.slides.length) % this.slides.length;
+  }
+
+  goToSlide(index: number) {
+    this.currentIndex = index;
+  }
+}
+
+
